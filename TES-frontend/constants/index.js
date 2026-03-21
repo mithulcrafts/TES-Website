b1171acbe2568de239a51a6f03237ed0cf766fed
Dhananjay <dhananjayagrawalmain001@gmail.com>
2026-03-21 13:22:14 +0530

    landingPage

---
+++ TES-frontend/constants/index.js
@@ -0,0 +1,26 @@
+import { Code, Trophy, Mic } from "lucide-react";
+
+export const heroCounts = [
+  { value: "1700+", label: "SOC 2025 Participation" },
+  { value: "110+", label: "Wiki Articles" },
+  { value: "90+", label: "GitHub Repos" },
+  { value: "18+", label: "Years since formation" },
+];
+
+export const heroCards = [
+  {
+    title: "Fast Paced Courses",
+    desc: "Deep dive into topics like machine learning, Web Development, and more",
+    icon: Code,
+  },
+  {
+    title: "Hackathons",
+    desc: "Compete with like minded developers",
+    icon: Trophy,
+  },
+  {
+    title: "Expert Talk Sessions",
+    desc: "Quick, intensive tech sessions",
+    icon: Mic,
+  },
+];
